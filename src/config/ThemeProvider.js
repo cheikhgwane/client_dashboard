395c1429fb7e0adf947a73cca6458daaf36cac52
cheikh <cheikhgwane@gmail.com>
2020-11-16 21:58:16 +0000

    create theme context provider

---
+++ src/config/ThemeProvider.js
@@ -0,0 +1,5 @@
+import React from "react"
+import { theme } from "./theme"
+
+
+export const ThemeContext = React.createContext(theme.light)
---
+++ src/config/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+    light: {
+        sideBar: {
+            color: "blue",
+            inverted: false,
+            itemColor: "",
+            iconColor: "blue"
+        }
+    },
+    dark: {
+        sideBar: {
+            color: "red",
+            inverted: true,
+            itemColor: "blue",
+            iconColor: "blue"
+        }
+    }
+}
